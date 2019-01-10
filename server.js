dd2e03c76e39e245c5e228e77c05e269bf920248
SarthakSri98 <sarthaksri1998@gmail.com>
2019-01-11 00:06:30 +0530

    third commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var app = require('./backend/app');
+
+const port = process.env.PORT | 8000 ;
+
+app.set('port',port);
+const server = http.createServer(app);
+
+server.listen(port,()=>{
+    console.log('The backend is initialised');
+})
