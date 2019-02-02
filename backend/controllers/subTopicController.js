e334592f6973f7e4c8915d8cb3a966c925490aa5
SarthakSri98 <sarthaksri1998@gmail.com>
2019-02-02 20:55:37 +0530

    error resolved for heroku application error

--- backend/controllers/subTopicController.js
+++ backend/controllers/subTopicController.js
@@ -12,7 +12,7 @@ exports.getSubTopic = function(req,res)
          topic : cb=>Topic.findById(mongoose.Types.ObjectId(req.query.id1)).exec(cb),
          subtopic : cb=> { SubTopic.find({ 'topicName':mongoose.Types.ObjectId(req.query.id1) }).populate('genreName').populate('topicName').exec(cb) }   
      },(err,result)=>{
-         console.log(result.subtopic[0].linkData)
+        //  console.log(result.subtopic[0].linkData)
          console.log('sdvsd',result);
          if(err) console.log(err);
          else
